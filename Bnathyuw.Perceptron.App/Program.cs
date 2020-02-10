dc571b18c65e378418d2ebeb1d202b4acd73bc07
Matthew Butt <matthew.butt@runpath.com>
2020-02-10 14:39:53 +0000

    Tidy up perceptron implementation

--- Bnathyuw.Perceptron.App/Program.cs
+++ Bnathyuw.Perceptron.App/Program.cs
@@ -1,68 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using static System.Math;
 
 namespace Bnathyuw.Perceptron.App
 {
     class Program
     {
+        private const double TrainingRate = 0.1d;
+
         static void Main(string[] args)
         {
-            const double trainingRate = 0.1d;
+            var weights = TrainNetwork();
+
+            TestNetwork(weights);
+        }
+        
+        private static double[] TrainNetwork()
+        {
             var trainingCases = File.ReadLines("Data/training_10_6_1.csv");
             var weights = new[] {0d, 0d, 0d};
-            foreach (var (input, region) in trainingCases.Select(ParseValues))
+            foreach (var (inputs, actualMatch) in trainingCases.Select(ParseValues))
             {
-                Console.WriteLine($"input {string.Join(',', input)}");
+                Console.WriteLine($"input {string.Join(',', inputs)}");
                 Console.WriteLine($"weights {string.Join(',', weights)}");
-                var output = Calculate(input, weights);
-                Console.WriteLine($"output: {output}, expected: {region}");
-                var newWeight0 = weights[0] + trainingRate * (region - output) * input[0];
-                var newWeight1 = weights[1] + trainingRate * (region - output) * input[1];
-                var newWeight2 = weights[2] + trainingRate * (region - output) * input[2];
-                weights = new[] {newWeight0, newWeight1, newWeight2};
+                var predictedMatch = Calculate(inputs, weights);
+                Console.WriteLine($"output: {predictedMatch}, expected: {actualMatch}");
+
+                double AdjustWeight(double input, double weight) =>
+                    weight + TrainingRate * (actualMatch - predictedMatch) * input;
+
+                weights = inputs.Zip(weights, AdjustWeight).ToArray();
             }
 
-            var testCases = File.ReadLines("Data/test_10_6_1.csv");
-            var correct = 0;
-            var incorrect = 0;
-            foreach (var (input, region) in testCases.Select(ParseValues))
-            {
-                var output = Calculate(input, weights);
-                if (output == region)
-                {
-                    Console.WriteLine("Correct!");
-                    correct++;
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect!");
-                    incorrect++;
-                }
-            }
-            Console.WriteLine($"Total correct {correct}; total incorrect {incorrect}");
+            return weights;
         }
 
-        private static int Calculate(double[] input, double[] weights)
+        private static void TestNetwork(double[] weights)
         {
-            return Math.Sign(input[0] * weights[0] + input[1] * weights[1] + input[2] * weights[2]);
+            var testCases = File.ReadLines("Data/test_10_6_1.csv");
+            var results = testCases.Select(ParseValues)
+                .Select(tuple => Calculate(tuple.input, weights) == tuple.region);
+
+            Console.WriteLine($"Total correct {results.Count(x => x)}; total incorrect {results.Count(x => !x)}");
         }
+
+        private static int Calculate(IEnumerable<double> inputs, IEnumerable<double> weights) =>
+            Sign(inputs.Zip(weights, Product).Sum());
+
+        private static double Product(double input, double weight) => input * weight;
 
         private static (double[] input, int region) ParseValues(string first)
         {
             var strings = first.Split(",");
             var x = double.Parse(strings[0]);
             var y = double.Parse(strings[1]);
-            var region = strings[2] == "A" ? 1 : -1;
-            return (new[]{1d, x, y}, region);
-
+            var match = strings[2] == "A" ? 1 : -1;
+            return (new[] {1d, x, y}, match);
         }
-        
-        
-    }
-
-    public enum Region
-    {
-        A, B
     }
 }
