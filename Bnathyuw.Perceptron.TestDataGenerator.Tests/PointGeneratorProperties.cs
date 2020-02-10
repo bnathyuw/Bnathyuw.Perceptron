6d2260823fb64fa77660707e28b9f5d0e702fea9
Matthew Butt <matthew.butt@runpath.com>
2020-02-10 11:30:42 +0000

    Generate both regions/moons

--- Bnathyuw.Perceptron.TestDataGenerator.Tests/PointGeneratorProperties.cs
+++ Bnathyuw.Perceptron.TestDataGenerator.Tests/PointGeneratorProperties.cs
@@ -1,17 +1,84 @@
+using System;
 using FluentAssertions;
 using FsCheck.Xunit;
+using static System.Math;
 
 namespace Bnathyuw.Perceptron.TestDataGenerator.Tests
 {
     public class PointGeneratorProperties
     {
+        private const double Radius = 10;
+        private const double Width = 6;
+        private const double Separation = 1;
+        private readonly PointGenerator _pointGenerator;
+
+        public PointGeneratorProperties()
+        {
+            _pointGenerator = new PointGenerator(Radius, Width, Separation);
+        }
+
         [Property]
         public void PointsShouldBeDifferent()
         {
-            var pointGenerator = new PointGenerator();
-            var point = pointGenerator.GeneratePoint();
-            var anotherPoint = pointGenerator.GeneratePoint();
+            var point = _pointGenerator.GeneratePoint();
+            var anotherPoint = _pointGenerator.GeneratePoint();
             point.Should().NotBe(anotherPoint);
+        }
+
+        [Property]
+        public void PointsShouldLieWithinRadius()
+        {
+            var point = _pointGenerator.GeneratePoint();
+            switch (point.Region)
+            {
+                case Region.A:
+                {
+                    var distanceFromCentre = Sqrt(point.X * point.X + point.Y * point.Y);
+                    distanceFromCentre.Should().BeLessOrEqualTo(Radius);
+                    break;
+                }
+                case Region.B:
+                {
+                    const double centreX = Radius - Width / 2d;
+                    const double centreY = -Separation;
+                    var distanceX = point.X - centreX;
+                    var distanceY = point.Y - centreY;
+                    var distanceFromCentre = Sqrt(distanceX * distanceX + distanceY * distanceY);
+                    distanceFromCentre.Should().BeLessOrEqualTo(Radius);
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        [Property]
+        public void PointsInRegionAShouldLieWithinWidthOfMoon()
+        {
+            var point = _pointGenerator.GeneratePoint();
+            switch (point.Region)
+            {
+                case Region.A:
+                {
+                    var distanceFromCentre = Sqrt(point.X * point.X + point.Y * point.Y);
+                    var distanceFromCircumference = Radius - distanceFromCentre;
+                    distanceFromCircumference.Should().BeLessOrEqualTo(Width);
+                    break;
+                }
+                case Region.B:
+                {
+                    const double centreX = Radius - Width / 2d;
+                    const double centreY = -Separation;
+                    var distanceX = point.X - centreX;
+                    var distanceY = point.Y - centreY;
+                    var distanceFromCentre = Sqrt(distanceX * distanceX + distanceY * distanceY);
+                    var distanceFromCircumference = Radius - distanceFromCentre;
+                    distanceFromCircumference.Should().BeLessOrEqualTo(Width);
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
--- Bnathyuw.Perceptron.TestDataGenerator/Program.cs
+++ Bnathyuw.Perceptron.TestDataGenerator/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Globalization;
+using static System.Math;
 
 namespace Bnathyuw.Perceptron.TestDataGenerator
 {
@@ -7,34 +7,74 @@ namespace Bnathyuw.Perceptron.TestDataGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pointGenerator = new PointGenerator(10, 6, 1);
+            
+            for (var i = 0; i < 2000; i++)
+            {
+                var point = pointGenerator.GeneratePoint();
+                Console.WriteLine(point);
+            }
         }
     }
 
     public class PointGenerator
     {
-        private Random _random;
+        private readonly double _radius;
+        private readonly double _width;
+        private readonly double _separation;
+        private readonly Random _random;
 
-        public PointGenerator()
+        public PointGenerator(double radius, double width, double separation)
         {
+            _radius = radius;
+            _width = width;
+            _separation = separation;
             _random = new Random();
         }
         public Point GeneratePoint()
         {
+            var region = (Region) _random.Next(2);
+            var distance = _radius - _random.NextDouble() * _width;
+            var (startAngle, centreX, centreY) = GetMetrics(region);
+            var angle = startAngle + _random.NextDouble() * PI;
+            var x = centreX + Cos(angle) * distance;
+            var y = centreY + Sin(angle) * distance;
             return new Point
             {
-                X = _random.Next(),
-                Y = _random.Next(),
-                Region = (Region) _random.Next(0,1)
+                X = x,
+                Y = y,
+                Region = region
             };
+        }
+
+        private (double startAngle, double centreX, double centreY) GetMetrics(Region region)
+        {
+            switch (region)
+            {
+                case Region.A:
+                {
+                    return (0, 0, 0);
+                }
+                case Region.B:
+                {
+                    return (PI, _radius - _width / 2, -_separation);
+                }
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 
     public struct Point
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
         public Region Region { get; set; }
+
+        public override string ToString()
+        {
+            return $"{X}, {Y}, {Region}";
+        }
     }
     
     public enum Region
